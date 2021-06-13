0a55d214f9c70b8621a120ea7d4b717b2bf8bebd
Kalaimani Balu <kalaimani.balu@gmail.com>
2021-06-13 14:53:18 +0530

    Move TreeNode struct & impl to separate file

---
+++ src/model.rs
@@ -0,0 +1,19 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: TreeNodeRef,
+  pub right: TreeNodeRef,
+}
+
+pub type TreeNodeRef = Option<Rc<RefCell<TreeNode>>>;
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32, left: TreeNodeRef, right: TreeNodeRef) -> TreeNodeRef {
+    Some(Rc::new(RefCell::new(TreeNode { val, left, right })))
+  }
+}
+
