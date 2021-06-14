616dad93c343b8ffd4f66a96cdcf4c98ea95419c
Kalaimani Balu <kalaimani.balu@gmail.com>
2021-06-14 16:47:28 +0530

    BFS algorithms

--- src/bfs.rs
+++ src/bfs.rs
@@ -48,3 +48,51 @@ pub fn right_side_view(root: TreeNodeRef) -> Vec<i32> {
 
     result
 }
+
+/// (2) BINARY TREE LEVEL ORDER TRAVERSAL
+///
+/// ```
+/// use algorithms::model::TreeNode;
+/// use algorithms::bfs::level_order;
+/// let node15 = TreeNode::new(15, None, None);
+/// let node7 = TreeNode::new(7, None, None);
+/// let node20 = TreeNode::new(20, node15, node7);
+/// let node9 = TreeNode::new(9, None, None);
+/// let node3 = TreeNode::new(3, node9, node20);
+///
+/// assert_eq!(vec![vec![3], vec![9, 20], vec![15, 7]], level_order(node3));
+/// ```
+pub fn level_order(root: TreeNodeRef) -> Vec<Vec<i32>> {
+    let mut result = vec![];
+    let mut queue = VecDeque::new();
+
+    if root.is_none() {
+        return result;
+    }
+
+    queue.push_back(vec![root]);
+
+    while !queue.is_empty() {
+        let mut values = vec![];
+        let mut nodes= vec![];
+
+        for node in queue.pop_front().unwrap() {
+            if let Some(node) = node {
+                let node = node.borrow();
+                values.push(node.val);
+                nodes.push(node.left.clone());
+                nodes.push(node.right.clone());
+            }
+        }
+
+        if !values.is_empty() {
+            result.push(values);
+        }
+
+        if !nodes.is_empty() {
+            queue.push_back(nodes);
+        }
+    }
+
+    result
+}
