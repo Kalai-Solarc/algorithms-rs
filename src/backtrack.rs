2ade7e94764a98b4c8662f81e9915ce5eebd976e
Kalaimani Balu <kalaimani.balu@gmail.com>
2021-06-20 15:48:40 +0530

    Code cleanup

--- src/backtrack.rs
+++ src/backtrack.rs
@@ -1,6 +1,57 @@
 use std::iter::FromIterator;
 use std::ops::Deref;
 
+
+/// LETTER COMBINATIONS
+///
+/// Given a string containing digits from 2-9 inclusive, return all possible letter combinations
+/// that the number could represent. Return the answer in any order.
+///
+/// A mapping of digit to letters (just like on the telephone buttons). Note that 1 does not map to any letters.
+///
+/// Examples:
+/// ```
+/// use algorithms::backtrack::letter_combinations;
+///
+/// assert_eq!(Vec::<String>::new(), letter_combinations(""));
+/// assert_eq!(vec!["a", "b", "c"], letter_combinations("2"));
+/// assert_eq!(vec!["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], letter_combinations("23"));
+/// ```
+///
+pub fn letter_combinations(digits: &str) -> Vec<String> {
+    let mut results = vec![];
+
+    if digits.is_empty() {
+        return results;
+    }
+
+    let pad = [
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
+    ];
+
+    let digits = digits.as_bytes();
+
+    fn dfs(digits: &[u8], buf: &mut String, index: usize, pad: &[&str], results: &mut Vec<String>) {
+        if digits.len() == index {
+            results.push(buf.to_owned());
+            return;
+        }
+
+        let lookup = (digits[index] - b'0') as usize;
+
+        for letter in pad[lookup].chars() {
+            buf.push(letter);
+            dfs(digits, buf, index + 1, pad, results);
+            buf.pop();
+        }
+    }
+
+    dfs(digits.as_ref(), &mut "".to_string(), 0, &pad, results.as_mut());
+
+    results
+}
+
+
 /// N QUEENS
 ///
 /// ```
--- src/depth_first_search.rs
+++ src/depth_first_search.rs
@@ -5,55 +5,6 @@ use std::rc::Rc;
 
 use crate::model::{TreeNode, TreeNodeRef, TreeNodeWithNextRef};
 
-/// LETTER COMBINATIONS
-///
-/// Given a string containing digits from 2-9 inclusive, return all possible letter combinations
-/// that the number could represent. Return the answer in any order.
-///
-/// A mapping of digit to letters (just like on the telephone buttons). Note that 1 does not map to any letters.
-///
-/// Examples:
-/// ```
-/// use algorithms::depth_first_search::letter_combinations;
-///
-/// assert_eq!(Vec::<String>::new(), letter_combinations(""));
-/// assert_eq!(vec!["a", "b", "c"], letter_combinations("2"));
-/// assert_eq!(vec!["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], letter_combinations("23"));
-/// ```
-///
-pub fn letter_combinations(digits: &str) -> Vec<String> {
-    let mut results = vec![];
-
-    if digits.is_empty() {
-        return results;
-    }
-
-    let pad = [
-        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",
-    ];
-
-    let digits = digits.as_bytes();
-
-    fn dfs(digits: &[u8], temp: String, index: usize, pad: &[&str], results: &mut Vec<String>) {
-        if digits.len() == index {
-            results.push(temp);
-            return;
-        }
-
-        let lookup = (digits[index] - b'0') as usize;
-
-        for letter in pad[lookup].chars() {
-            let mut buf = String::with_capacity(temp.len() + 1);
-            buf.push_str(&temp);
-            buf.push(letter);
-            dfs(digits, buf, index + 1, pad, results)
-        }
-    }
-
-    dfs(digits.as_ref(), "".to_string(), 0, &pad, results.as_mut());
-
-    results
-}
 
 /// VALID BINARY TREE
 ///
